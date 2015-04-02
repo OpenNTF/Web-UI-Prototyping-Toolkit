f40342e404f416f0fc45369272be33582126ef9c
Peter Grauvogel <peter@de.ibm.com>
2015-04-02 15:09:52 +0200

    Initial markup for the sub generator to create modules.

--- core/yeoman/generator-wuipt/app/index.js
+++ core/yeoman/generator-wuipt/app/index.js
@@ -13,7 +13,7 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the transcendent ' + chalk.red('Wuipt') + ' generator!'
+      'Welcome to the transcendent ' + chalk.red('WUIPT') + ' generator!'
     ));
 
     var prompts = [{
