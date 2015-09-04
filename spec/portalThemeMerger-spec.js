4ce091e5b89321988536bf9b43a093cbb13b3236
Jonathan Bosmans <jonathan.bosmans@be.ibm.com>
2015-09-04 12:14:25 +0200

    First portal theme merge automation steps : static files 1. copies the prototype 2. builds prototype 3. copies build over prototype copy 4. copies portal theme 5. integrates processed prototype into theme copy 6. pushes configred merge paths into theme copy

---
+++ spec/portalThemeMerger-spec.js
@@ -0,0 +1,80 @@
+var path = require("path");
+//var protostarBuilder = require("../lib/protostarBuilder");
+var testUtils = require("../lib/testUtils");
+var utils = require("../lib/utils");
+var protostarProject = require("../lib/protostarProject");
+var templateComposer = require("../lib/templateComposer");
+var protostarBuilder = require("../lib/protostarBuilder");
+var portalThemeMerger = require("../lib/portalThemeMerger");
+var originalTimeout;
+
+//if(false)
+describe("portalThemeMerger", function(){
+    beforeEach(function() {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+    });
+
+    it("can run", function(done){
+        var dsvPrototypePath = '/home/spectre/Projects/IBM/DSV/mydsv-protostar/mydsv';
+        var dsvThemePath = '/home/spectre/Projects/IBM/DSV/angularTheme';
+
+        //var sampleProjectPath = path.join(__dirname, "../projects/sample")
+        var runtime = testUtils.createTestRuntime(dsvPrototypePath);
+        var targetDir = "/tmp/psMergeThemeTest_" + (new Date().getTime());
+        runtime.targetDirPath = targetDir;
+        runtime.targetDir = targetDir;
+        var composer = templateComposer.createTemplateComposer({
+            runtime : runtime
+        });
+        var project = protostarProject.createProject({
+            runtime:runtime,
+            composer:composer
+        });
+
+
+
+        var builder = protostarBuilder.createBuilder({
+            runtime : runtime,
+            project : project,
+            composer :composer,
+            targetDir : targetDir,
+            ignoreExcludeFromBuild : false
+        });
+
+        portalThemeMerger.merge({
+            targetDir : targetDir,
+            projectPath : dsvPrototypePath,
+            themePath : dsvThemePath,
+            runtime:runtime,
+            composer:composer,
+            project:project,
+            builder:builder
+        }, function(){
+            console.log("success");
+            done();
+        }, function(errors){
+            console.log("error ::: ",errors);
+            done();
+        });
+
+
+        //builder.createZipBuild(function(zip, targetDir, dirName){
+        //    console.log("TARGET DIR = "+ targetDir)
+        //    console.log("dirName = "+ dirName);
+        //    zip.writeZip(targetDir + ".zip");
+        //    var foundCss = false;
+        //    zip.getEntries().forEach(function(e){
+        //        console.log(e.name);
+        //        if(e.name.toString() === 'styles.less-readable.css'){
+        //            foundCss = true;
+        //        }
+        //    });
+        //    expect(foundCss).toBe(true);
+        //    done();
+        //}, function(error){
+        //    console.error("BUILD ERRORS", error.stack);
+        //    done();
+        //});
+    });
+});
