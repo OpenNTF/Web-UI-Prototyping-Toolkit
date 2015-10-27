7c792138ab1af05901132fcf71c9e8383c5a0e1c
Jonathan Bosmans <jonathan.bosmans@be.ibm.com>
2015-10-28 00:01:26 +0100

    Initial uncss support: add ?onlyUsed to a transparant less css tag (script tag pointing to css file for which only a less equiv exists at that place)

--- spec/cssFixess-spec.js
+++ spec/cssFixess-spec.js
@@ -69,7 +69,7 @@ describe("File Oriented Templating Language", function(){
 
         uncss(files, options,function (error, output) {
             var newLength = output.length;
-            console.log("Went from " + inLength.length + " to " + newLength)
+            console.log("Went from " + inLength.length + " to " + newLength);
             done();
         });
 
@@ -83,9 +83,9 @@ describe("File Oriented Templating Language", function(){
     });
 
 
-    fit("can retain only used styles in pages", function(done){
+    it("can retain only used styles in pages", function(done){
 
-        var fd = path.resolve(__dirname, "files/usedStyles")
+        var fd = path.resolve(__dirname, "files/usedStyles");
 
         //var uncss = require('uncss');
 
@@ -154,5 +154,81 @@ describe("File Oriented Templating Language", function(){
         //    console.log(output);
         //    done()
         //});
+    });
+
+    it("can retain only used styles in pages", function(done){
+
+        //var fd = path.resolve(__dirname, "files/usedStyles");
+
+        //var uncss = require('uncss');
+
+        //var files   = ['my', 'array', 'of', 'HTML', 'files', 'or', 'http://urls.com'];
+        //var files   = [
+        //    "/tmp/testbuild/blog.html",
+        //    "/tmp/testbuild/carousel.html",
+        //    "/tmp/testbuild/contacts.html",
+        //    "/tmp/testbuild/grid.html",
+        //    "/tmp/testbuild/index.html",
+        //    "/tmp/testbuild/jumbotron.html",
+        //    "/tmp/testbuild/minimal.html",
+        //    "/tmp/testbuild/questions.html",
+        //    "/tmp/testbuild/solution.html",
+        //    "/tmp/testbuild/solutions.html",
+        //    "/tmp/testbuild/test.html"
+        //];
+        //var styleSheet = path.resolve(fd, "styles.css");
+        //var origFile = fs.readFileSync(styleSheet, 'utf8');
+
+        var options = {
+            //ignore       : ['#added_at_runtime', /test\-[0-9]+/],
+            //media        : ['(min-width: 700px) handheld and (orientation: landscape)'],
+            //csspath      : '../public/css/',
+            //raw          : 'h1 { color: green }',
+            stylesheets  : ['/styles.css'],
+            //ignoreSheets : [/fonts.googleapis/],
+            timeout      : 1000,
+            //htmlroot     : fd,
+            report       : true,
+            //uncssrc      : '.uncssrc'
+        };
+
+        //cssFixes.removeUnusedCss([fd + path.sep + "index.html"], options, function(err, output, report){
+        //    if(err){
+        //        console.error("ERROR ", err.stack);
+        //    }else{
+        //        var newLength = output.length;
+        //        fs.writeFileSync(styleSheet + ".mod.css", output);
+        //        console.log("Went from " + origFile.length + " to " + newLength);
+        //        console.log("REPORT = ", report);
+        //        for(var k in report){
+        //            console.log("key = " + k);
+        //        }
+        //
+        //    }
+        //    done();
+        //
+        //});
+
+        //uncss(files, options, function (error, output) {
+        //    console.log(output);
+        //});
+
+        //uncss(files, options,function (error, output) {
+        //
+        //    //console.log(arguments[2]);
+        //    var newLength = output.length;
+        //    console.log("Went from " + inLength.length + " to " + newLength)
+        //    done();
+        //});
+
+        var rawHtml = '<html><head></head><body><div class="yow">hello</div></body></html>';
+        //
+        uncss(rawHtml, {
+            raw : '.yow { color:red;} p {color:white}'
+        }, function (error, output) {
+            console.log(output);
+            done()
+        });
     })
+
 });
